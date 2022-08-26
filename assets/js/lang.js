eb14cb552886c38e685e5af6eeb32b18711dfe62
Ömer Faruk <bayomerfg@gmail.com>
2022-08-26 16:47:15 +0300

    information

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,6 +1,6 @@
 var arrLang={
     'en':{
-      'about' : 'My name is Omer Faruk GUNES. I am a fourth year computer engineer student at Cukurova University. I love working at a computer. I have always been interested in technology and researching since my childhood. Especially, I enjoy working on technologies that appeal to the end user. Learning and discovering new things have become a hobby for me. In addition, I love helping my teammates in teamwork and personally improving myself from their experiences. In my spare time I ride a bike, play basketball or watch movies.',
+      'about' : 'My name is Omer Faruk GUNES. I graduated from Çukurova University, department of Computer Engineering. I love working at a computer. I have always been interested in technology and researching since my childhood. Especially, I enjoy working on technologies that appeal to the end user. Learning and discovering new things have become a hobby for me. In addition, I love helping my teammates in teamwork and personally improving myself from their experiences. In my spare time I ride a bike, play basketball or watch movies.',
       'MaarifYear' : 'Jun 2021 - July 2021',
       'ImaginiteYear' : 'October 2021 - Jun 2022',
       'MarulaYear' : 'Jun 2022 - Present',
@@ -72,7 +72,7 @@ var arrLang={
       'Info' : ' *This table represents my educational process according to my projects.'
     },
     'tr':{
-      'about' : 'Adım Ömer Faruk GÜNEŞ. 4.Sınıf Çukurova Universitesi Bilgisayar Mühendisi öğrencisiyim. Bilgisayar başında çalışmaktan çok büyük keyif alıyorum. Teknoloji çocukluğumdan beri ilgimi çekmiştir. Özellikle son kullanıcıya hitap eden teknolojiler üzerinde çalışmaktan büyük zevk almaktayım. Yeni bilgiler edinmek durmadan araştırmalar yapmak benim için bir hobi haline gelmiş durumda. Tüm bunlara ek olarak, bir takım çalışmasında takım arkadaşlarıma yardım etmeyi ve onların tecrübelerinden kişisel olarak gelişim sağlamayı çok severim. Boş zamanlarımı bisiklet sürerek, basketbol oynarak veya film ve dizi izleyerek geçiririm.',
+      'about' : 'Adım Ömer Faruk GÜNEŞ. Çukurova Üniversitesi bilgisayar mühendisliği bölümünden mezun oldum. Bilgisayar başında çalışmaktan çok büyük keyif alıyorum. Teknoloji çocukluğumdan beri ilgimi çekmiştir. Özellikle son kullanıcıya hitap eden teknolojiler üzerinde çalışmaktan büyük zevk almaktayım. Yeni bilgiler edinmek durmadan araştırmalar yapmak benim için bir hobi haline gelmiş durumda. Tüm bunlara ek olarak, bir takım çalışmasında takım arkadaşlarıma yardım etmeyi ve onların tecrübelerinden kişisel olarak gelişim sağlamayı çok severim. Boş zamanlarımı bisiklet sürerek, basketbol oynarak veya film ve dizi izleyerek geçiririm.',
       'ImaginiteYear' : 'Ekim 2021 - Haziran 2022',
       'MarulaYear' : 'Haziran 2022 - Devam',
       'MaarifYear' : 'Haziran 2021 - Temmuz 2021',
